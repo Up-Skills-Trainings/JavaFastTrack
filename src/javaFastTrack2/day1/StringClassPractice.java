6d57b694f6997214a38eba52c0d628fec467bc6f
nac1s <naci@cybertekschool.com>
2021-12-04 12:02:43 -0600

    day2 added

--- src/javaFastTrack2/day1/StringClassPractice.java
+++ src/javaFastTrack2/day1/StringClassPractice.java
@@ -3,21 +3,29 @@ package course2_day1;
 public class StringClassPractice {
 	public static void main(String[] args) {
 		String str = "hello"; // string is a char array ['h', 'e', 'l', 'l', 'o']
-		
+
 		// What is String in Java? String is a data type?
 		// String is the most important class of the Java.
-		// It has around 13 constructors and more than 40 methods.
-		// It is a final and immutable(not changeable) class. 
-		
+		// It has around 13 constructors and more than 47 methods.
+		// It is a final and immutable(not changeable) class.
+		// Stringbuilder and Stringbuffer >> mutable classes
+
 		// What are different ways to create String Object?
-		
+
 		String str1 = "hello"; // obj1
-		
+
 		String str2 = new String("hello");
-		
+
 		String str3 = "hello";
-		
+
 		// == >> it points out the memory location
 		// equals() >> it point out the value
+
+		char[] helloArray = { 'h', 'e', 'l', 'l', 'o', '.' };
+		String helloString = new String(helloArray);
+		System.out.println(helloString);
+
+		// Common string methods
+
 	}
 }
