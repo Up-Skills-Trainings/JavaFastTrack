0d6d4883ee4ae70119782479529584c63058a20b
nac1s <naci@cybertekschool.com>
2021-12-05 08:47:23 -0600

    updated

--- src/javaFastTrack2/day03/Day2Review.java
+++ src/javaFastTrack2/day03/Day2Review.java
@@ -2,29 +2,7 @@ package javaFastTrack2.day03;
 
 public class Day2Review {
 	public static void main(String[] args) {
-		// Palindrome
-		recursive(10);
-
-		int a = 10;
-		int b = 8;
-		System.out.println(sum(a, b));
-	}
-
-	static int sum(double a, int b) {
-		return (int) (a + b);
-	}
-
-	static int sum(int a, double b) {
-		return (int) (a + b);
-	}
-
-	static void recursive(int a) {
-
-		if (a == 15) {
-			return;
-		}
-		System.out.println(a++);
-		recursive(a);
+	
 	}
 
 }
