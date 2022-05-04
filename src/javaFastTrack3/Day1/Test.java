275e5f228063b8147756730e3b9613ec77461f73
Naci Gungor <naci@cybertekschool.com>
2022-05-04 16:43:29 -0500

    test

--- src/javaFastTrack3/Day1/Test.java
+++ src/javaFastTrack3/Day1/Test.java
@@ -1,5 +1,10 @@
 package javaFastTrack3.Day1;
 
 public class Test {
-
+	public static void main(String[] args) {
+		String str = "hello world";
+		
+		System.out.println(str.indexOf("hello"));
+		
+	}
 }
